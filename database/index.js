a056c1266f03cd75256bc1571e3196e8f0d62f04
marcellino-ornelas <marcellino.ornelas.com>
2018-08-24 17:01:59 -0700

    finsihed uploading docker images to ec2

--- database/index.js
+++ database/index.js
@@ -6,6 +6,10 @@ console.log(process.env.PGHOST);
 
 const pool = new pg.Pool();
 
+pool.on('connect', function() {
+  process.env.VERBOSE && console.log('client connected');
+});
+
 pool.on('error', function(err) {
   pool.end();
   console.log('There was a error with pg', err);
@@ -17,12 +21,19 @@ module.exports = pool;
 pool.connectAndEnd = function(action) {
   return new promise(function(resolve, reject) {
     pool.connect(function(err, client) {
+      process.env.VERBOSE && console.log('connecting and end', err, client);
+      if (err) {
+        return reject(err);
+      }
+
       return action(client)
         .then(function(res) {
+          process.env.VERBOSE && console.log('action good:', res);
           client.release();
           return resolve(res);
         })
         .catch(function(err) {
+          process.env.VERBOSE && console.log('action bad:', err);
           client.release();
           return reject(err);
         });
